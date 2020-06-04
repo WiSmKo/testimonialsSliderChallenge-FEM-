7f4dfed6b7aa60ad7aadad19488e906d6d4545d8
Billy Smallman-Köpf <wsmallmankoepf@outlook.com>
2020-06-04 13:08:01 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-const john = document.getElementById("john");
-const tanya = document.getElementById("tanya");
-const nav = document.querySelectorAll(".nav");
+const photo = [].slice.call(document.querySelectorAll(".profile"));
+const testimonial = [].slice.call(document.querySelectorAll(".quotecontainer"));
+const nav = document.getElementById("nav");
 
-//adds and removes the hidden class to show the one not currently displayed
+//function that loops through each photo item and toggles the hidden class
+
 function change(){
-    tanya.classList.toggle("hidden");
-    john.classList.toggle("hidden");
+    for (i=0; i < photo.length; i++){
+        console.log(photo)
+        photo[i].classList.toggle("hidden");
+        testimonial[i].classList.toggle("hidden");
+    }
 }
 
-//To tie buttons position relative to the images they had to be child elements of the images
-//so each image has it's own set of buttons, nav here represents a node list of all the buttons
-//here we loop through each set of navigation buttons and apply a click event listener
-nav.forEach(
-    function(){
-        this.addEventListener("click", change)
-    });
+//calls the change function when the nav is clicked
+nav.addEventListener("click", change);
